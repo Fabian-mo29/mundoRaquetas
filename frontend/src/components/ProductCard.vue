fa8d312f711800e9b59658b41a380e7a9ab36371
unknown <fabian.moraarias@ucr.ac.cr>
2025-04-25 14:49:23 -0600

    Se realiza el enrutamiento e inicializacion de la vista para los detalles del producto

--- frontend/src/components/ProductCard.vue
+++ frontend/src/components/ProductCard.vue
@@ -1,28 +1,36 @@
 <template>
-    <img :src="`/imagesProducts/${product.image}`" class="card-img-top img-ratio" :alt="'Imagen de ' + product.name" />
-      <div class="card-body">
-        <p class="card-text"><strong>{{ product.name }}</strong></p>
-        <p class="card-text">{{ product.Descripcion }}</p>
-        <div class="d-flex justify-content-between align-items-center">
-          <p style="font-size: large;"><strong>${{ product.precio }}</strong></p>
-          <button class="btn btn-success">Comprar</button>
-        </div>
-      </div>
+  <img
+    :src="`/imagesProducts/${product.image}`"
+    class="card-img-top img-ratio"
+    :alt="'Imagen de ' + product.name"
+  />
+  <div class="card-body">
+    <p class="card-text">
+      <strong>{{ product.name }}</strong>
+    </p>
+    <p class="card-text">{{ product.Descripcion }}</p>
+    <div class="d-flex justify-content-between align-items-center">
+      <p style="font-size: large">
+        <strong>${{ product.precio }}</strong>
+      </p>
+      <RouterLink :to="'product/' + product.id"
+        ><button class="btn btn-success">Comprar</button></RouterLink
+      >
+    </div>
+  </div>
 </template>
 
 <script setup>
-
-import { defineProps } from 'vue';
+import { defineProps } from "vue";
+import { RouterLink } from "vue-router";
 
 defineProps({
   limite: {
     type: Number,
-    default: 6 
+    default: 6,
   },
-  product: Object
+  product: Object,
 });
-
-
 </script>
 
 <style lang="scss" scoped>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue';
-import RegisterView from '@/views/RegisterView.vue';
-import LogInView from '@/views/LogInView.vue';
-import NotFoundView from '@/views/NotFoundView.vue';
-import ShoppingCartView from '@/views/ShoppingCartView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "@/views/HomeView.vue";
+import RegisterView from "@/views/RegisterView.vue";
+import LogInView from "@/views/LogInView.vue";
+import NotFoundView from "@/views/NotFoundView.vue";
+import ShoppingCartView from "@/views/ShoppingCartView.vue";
+import ProductDetailsView from "@/views/ProductDetailsView.vue";
+import ProductOfertasView from "@/views/ProductOfertasView.vue";
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory("/"),
   routes: [
     {
       path: "/",
@@ -24,14 +26,26 @@ const router = createRouter({
       component: LogInView,
     },
     {
-      path: '/:pathMatch(.*)*',
-      name: "NotFound",
-      component: NotFoundView,
-    },
-    {
       path: "/shoppingCart",
       name: "ShoppingCart",
       component: ShoppingCartView,
+    },
+    {
+      path: "/product/:id",
+      name: "Product",
+      component: ProductDetailsView,
+      props: (route) => ({ productId: Number(route.params.id) }),
+    },
+    {
+      path: "/oferta/:id",
+      name: "Oferta",
+      component: ProductOfertasView,
+      props: (route) => ({ ofertaId: Number(route.params.id) }),
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: NotFoundView,
     },
   ],
 });
