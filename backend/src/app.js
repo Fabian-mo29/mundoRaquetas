267bf5fd21d895fecea77c006dc9445a544e5c1c
Fabian <fabian.moraarias@ucr.ac.cr>
2025-04-30 15:42:01 -0600

    Inicializacion del backend para el proyecto

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+// require("dotenv").config();
+
+const app = express();
+const apiRoutes = require("./routes/api");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", apiRoutes);
+
+const PORT = 3000; // Cambia el puerto si es necesario
+app.listen(PORT, () => {
+  console.log(`Servidor backend corriendo en http://localhost:${PORT}`);
+});
