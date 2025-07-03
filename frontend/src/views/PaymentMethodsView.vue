ccf24aa713d84f8cbf0a7979231112d6ee2ab054
AlvaroMoya28 <alvaromoya2804@gmail.com>
2025-07-03 01:06:29 -0600

    Save card

---
+++ frontend/src/views/PaymentMethodsView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <PaymentMethods/>
+    </div>
+</template>
+
+<script setup>
+import PaymentMethods from '@/components/PaymentMethods.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
